37781d271e8a7bdceae7dd7960734f39ae4b3581
biezhi <biezhi.me@gmail.com>
2017-03-02 01:35:13 +0800

    🦄 fix plugin code

--- src/main/java/com/tale/Application.java
+++ src/main/java/com/tale/Application.java
@@ -1,8 +1,13 @@
 package com.tale;
 
+<<<<<<< 60abd17a13b4badda4bbb9894bb207c3afb281ba
 import com.blade.config.BConfig;
 import com.blade.kit.FileKit;
 import com.tale.controller.admin.AttachController;
+=======
+import com.tale.init.TaleLoader;
+import com.tale.utils.ExtClasspathLoader;
+>>>>>>> 🦄 fix plugin code
 
 import java.io.File;
 
@@ -10,6 +15,7 @@ import static com.blade.Blade.$;
 
 public class Application {
 
+<<<<<<< 60abd17a13b4badda4bbb9894bb207c3afb281ba
     public static void main(String[] args) {
         BConfig bConfig = $().bConfig();
         String themeDir = AttachController.CLASSPATH + "templates/themes";
@@ -19,6 +25,11 @@ public class Application {
                 bConfig.addStatic(new String[]{"/templates/themes/"+ f.getName() +"/static"});
             }
         }
+=======
+    public static void main(String[] args) throws Exception {
+        TaleLoader.init();
+        TaleLoader.loadPlugin(new File("/Users/biezhi/workspace/projects/java/plugin_upyun/target/plguin_upyun.jar"));
+>>>>>>> 🦄 fix plugin code
         $().start(Application.class);
     }
 
--- src/main/resources/static/admin/js/base.js
+++ src/main/resources/static/admin/js/base.js
@@ -36,7 +36,7 @@ $.tale.prototype.alertOkAndReload = function (text) {
     this.alertOk({text:text, then:function () {
         setTimeout(function () {
             window.location.reload();
-        }, 500);
+        }, 700);
     }});
 };
 
